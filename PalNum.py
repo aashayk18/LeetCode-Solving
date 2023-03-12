ddd85d9f71c7e5fc792c629c0d2a9ff8e80a7948
aashayk18 <aashayk100@gmail.com>
2023-03-12 16:42:49 +0530

    leetcode additional

---
+++ PalNum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        rev = str(x)[::-1]
+        if (str(x) ==  rev):
+            return True
+        else:
+            return False
+        return             
