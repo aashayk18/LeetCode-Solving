75db45adde9b48418f87fe335c9ceaeecbe68b4c
Aashay Kulkarni <72932792+aashayk18@users.noreply.github.com>
2023-11-01 11:24:26 +0530

    Create Tenth Line.sh

---
+++ Tenth Line.sh
@@ -0,0 +1,4 @@
+# Read from the file file.txt and output the tenth line to stdout.
+sed -n '10p' file.txt
+
+# This code uses the sed command to print the 10th line of the file. The -n option is used to suppress the default output and '10p' is used to print the 10th line of the file. 
